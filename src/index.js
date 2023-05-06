42ac066e844a45cb8f220a86ef6ec910a52db39b
Brayhan Suarez <brayhan9505@gmail.com>
2023-05-06 00:28:58 -0500

    update: config routes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 // Packages
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider, createHashRouter } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 // Routes
 import { routes } from "./routes";
 
 // Create Router
-const router = createHashRouter(routes);
+const router = createBrowserRouter(routes);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
